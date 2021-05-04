9265a07d12f46edd0506289b0342db3e62060a27
shenchaosong <shenchaosong@yidaoit.cn>
2021-05-04 18:03:24 +0800

    updagte

--- comp/basic/test.c
+++ comp/basic/test.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 void main(){
+	/*
 	int x = -1;
 	unsigned u = 2147483648;
-	
+	short si = 32768;
 	printf("x = %u = %d\n ", x,x);
 	printf("u = %u = %d\n ", u,u);
+	printf("si = %u = %d\n ", si,si);
+*/
+
+	unsigned int x = 134;
+	unsigned int y = 246;
+	int m = x;
+	int n = y;
+	unsigned int z1 = x-y;
+	unsigned int z2 = x+y;
+	int k1 = m - m;
+	int k2 = m + n;
+	printf("z1 = %u\n",z1);
+	printf("z2=%u\n",z2);
+	printf("k1=%d\n",k1);
+	printf("k2 = %d\n",k2);
 }
+
 
